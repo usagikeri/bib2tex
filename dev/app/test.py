aa8cbe3dcba1588b08bc330ee4336d957a894204
Usagikeri <a.k.a.usagikeri@gmail.com>
2018-07-17 19:09:30 +0900

    modify explain

--- dev/app/test.py
+++ dev/app/test.py
@@ -1,22 +1,34 @@
+"""
+$python test.py https://bib2tex.arukascloud.io                                                                                                                                   +[master]
+\begin{thebibliography}{9}
+        \bibitem{} 
+\end{thebibliography}
+"""
+
 import requests
 import json
+import sys
 
-hearder = {'content-type': 'application/json'}
-url = 'http://127.0.0.1:5000'
-data = json.dumps({'items': '''@article{170000088750,
-                author="藤井雄太郎 and 安藤哲志 and 伊藤孝行",
-                title="複数単語間の距離情報及び共起情報を利用した文書分類手法の提案",
-                journal="第73回全国大会講演論文集",
-                ISSN="",
-                publisher="",
-                year="2011",
-                month="mar",
-                volume="2011",
-                number="1",
-                pages="397-398",
-                URL="https://ci.nii.ac.jp/naid/170000088750/",
-                DOI="",
-                }'''})
+if __name__ == '__main__':
 
-r = requests.post(headers=hearder, url=url, data=data)
-print(r.content.decode())
+    args = sys.argv
+
+    hearder = {'content-type': 'application/json'}
+    url = args[1]
+    data = json.dumps({'items': '''@article{170000088750,
+                    author="usagikeri",
+                    title="論文タイトル",
+                    journal="Github",
+                    ISSN="",
+                    publisher="",
+                    year="2018",
+                    month="mar",
+                    volume="2018",
+                    number="1",
+                    pages="01-08",
+                    URL="https://github.com/usagikeri",
+                    DOI="",
+                    }'''})
+    
+    r = requests.post(headers=hearder, url=url, data=data)
+    print(r.content.decode())
